45a099fdb9003110a0ece501427a229003a3f232
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-06 14:32:37 +0200

    rest in python

---
+++ corey_schafer/list.py
@@ -0,0 +1,25 @@
+courses = ['history', 'maths', ' physics']
+courses_2 = ["science", "art"]
+# print(len(courses))
+# print(courses[0])
+# print(courses[0:3])
+# lists methods
+
+# use the append method for adding to the end of list
+# courses.append("article")
+
+# use insert method for adding to specific location
+# courses.insert(1, "alana")
+# use the extend method for adding to multiple values
+
+# courses.extend(courses_2)
+
+# remove values from list
+
+# courses.remove('maths')
+# courses.pop()   // this remove the last value from the end
+
+# sorting list
+# // reversed methods
+courses.reverse()
+print(courses)
