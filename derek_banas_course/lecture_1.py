1fbadb8d35753b63788f9abc7e55b2e218b5e1ff
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-06 04:21:11 +0200

    problem 4

--- derek_banas_course/lecture_1.py
+++ derek_banas_course/lecture_1.py
@@ -50,3 +50,27 @@
 # print results using format()
 # print("{} miles equals {} kilometers".format(miles, kilometers))
 
+
+
+# enter calculation: 5 * 6
+# 5 * 6 = 30
+
+# store the user input of 2 numbers and the operator
+num1, operator, num2 = input('Enter Calculation').split()
+
+# convert the strings into integers
+num1 = int(num1)
+num2 = int(num2)
+
+# if  + then we need to provide output based on addition
+
+if operator == "+":
+    print("{} + {} = {}".format(num1, num2, num1 + num2))
+elif operator == "-":
+    print("{} - {} = {}".format(num1, num2, num1 - num2))
+elif operator == "*":
+    print("{} * {} = {}".format(num1, num2, num1 * num2))
+elif operator == "/":
+    print("{} / {} = {}".format(num1, num2, num1 / num2))
+else:
+    print("use either + - * / next time")
