ad8aab7b23cb478c3b2ab297b348603da7445725
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-06 03:49:13 +0200

    12

---
+++ derek_banas_course/lecture_1.py
@@ -0,0 +1,6 @@
+#ask the user to input their name and assign it to a variable named name
+
+name = input('What is your name ')
+
+# print out their name followed by the name they entered.
+print('Hello ', name)
