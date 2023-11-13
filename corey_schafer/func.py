b3d9d02e84be3164b38dfe6f5cf7ad0e5481cde0
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-13 14:25:16 +0200

    python is cool

--- corey_schafer/func.py
+++ corey_schafer/func.py
@@ -1,20 +1,27 @@
-# def hello_func():
-#     print("Hello function thank you for been one")
+# # def hello_func():
+# #     print("Hello function thank you for been one")
 
-# hello_func()
+# # hello_func()
 
-# #DRY concept
+# # #DRY concept
 
-# def hello_me(greeting):
-#     return '{} function'.format(greeting)
+# # def hello_me(greeting):
+# #     return '{} function'.format(greeting)
 
-# print(hello_me("hello"))
+# # print(hello_me("hello"))
 
-def student_info(*args, **kwargs):
-    print(args)
-    print(kwargs)
+# def student_info(*args, **kwargs):
+#     print(args)
+#     print(kwargs)
     
-courses = ['math', 'Physics']
-info = {'name': 'joshua', 'age': 12}
+# courses = ['math', 'Physics']
+# info = {'name': 'joshua', 'age': 12}
 
-student_info(*courses, **info)
+# student_info(*courses, **info)
+
+
+month_days = [0,31,28,31,30,31,30,31,30,31,30,]
+
+def is_leap(year):
+    # return true if year is leap
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 != 0)
--- corey_schafer/use_module.py
+++ corey_schafer/use_module.py
@@ -1,1 +1,5 @@
 import my_module
+
+courses = ['math', 'css', 'c++', 'python', 'java']
+index = my_module.find_index(courses, 'css')
+print(index)
