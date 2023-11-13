d8eb6c7371acbeef0a04791c719494748a8b048a
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-13 14:43:43 +0200

    python is coo

--- corey_schafer/use_module.py
+++ corey_schafer/use_module.py
@@ -1,4 +1,7 @@
 import random
+import datetime
+import calendar
+
 from my_module import find_index
 
 courses = ['math', 'css', 'c++', 'python', 'java']
@@ -8,3 +11,6 @@ print(index)
 # getting random course
 random_course = random.choice(courses)
 print(random_course)
+
+todays = datetime.date.today()
+print(todays)
