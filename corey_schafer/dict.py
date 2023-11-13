61f3051cf7c5923af56f8d81f6849105e376ec11
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-13 13:18:45 +0200

    python is cool

--- corey_schafer/dict.py
+++ corey_schafer/dict.py
@@ -2,3 +2,7 @@ student = {'name': 'joshua', 'age': 13}
 print(student.get('name'))
 student.update({'name': 'alana', 'age': 12})
 print(student)
+print(len(student))
+
+for key, value in student.items():
+    print(key, value)
