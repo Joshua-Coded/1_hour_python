278956e3c2cb8fadc7c6b589817911bbde7eebe0
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-13 14:41:15 +0200

    python is coo

--- corey_schafer/use_module.py
+++ corey_schafer/use_module.py
@@ -1,5 +1,10 @@
+import random
 from my_module import find_index
 
 courses = ['math', 'css', 'c++', 'python', 'java']
 index = find_index(courses, 'css')
 print(index)
+
+# getting random course
+random_course = random.choice(courses)
+print(random_course)
