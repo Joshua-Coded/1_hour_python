d0e0019ece5c17fdc9d162c983e69bbe3c87b796
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-14 01:30:50 +0200

    python is cool

--- derek_banas_course/lecture_2.py
+++ derek_banas_course/lecture_2.py
@@ -1,14 +1,15 @@
-# learn to program in python with derek banas
+# # learn to program in python with derek banas
+# # write a program that prints odds from range of 1-21
+# #solution
+# # use for loop
+# for i in range(1, 21):
 
-# write a program that prints odds from range of 1-21
+# # use modulos to check that the result is not equal to zero
+#     if ((i % 2) != 0):
+# # print the odds
+#         print("i = ",  + i)
 
-#solution
-
-# use for loop
-for i in range(1, 21):
-
-# use modulos to check that the result is not equal to zero
-    if ((i % 2) != 0):
-
-# print the odds
-        print("i = ",  + i)
+# working with foating numbers
+your_float = input("Enter a float value: ")
+your_float = float(your_float)
+print("Rounded to 2 decimal : {:.2f}".format(your_float))
