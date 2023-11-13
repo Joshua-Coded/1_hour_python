67a76f89a4f2d247e8dc744a93b1e51acb78dc6c
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-13 14:03:35 +0200

    python is cool

--- corey_schafer/func.py
+++ corey_schafer/func.py
@@ -1,4 +1,20 @@
-def hello_func():
-    print("Hello function thank you for been one")
+# def hello_func():
+#     print("Hello function thank you for been one")
 
-hello_func()
+# hello_func()
+
+# #DRY concept
+
+# def hello_me(greeting):
+#     return '{} function'.format(greeting)
+
+# print(hello_me("hello"))
+
+def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+    
+courses = ['math', 'Physics']
+info = {'name': 'joshua', 'age': 12}
+
+student_info(*courses, **info)
