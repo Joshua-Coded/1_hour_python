edb58637f53b0fb1d37eeda707981d89c9827a0d
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-14 01:27:28 +0200

    python is cool

---
+++ derek_banas_course/lecture_2.py
@@ -0,0 +1,14 @@
+# learn to program in python with derek banas
+
+# write a program that prints odds from range of 1-21
+
+#solution
+
+# use for loop
+for i in range(1, 21):
+
+# use modulos to check that the result is not equal to zero
+    if ((i % 2) != 0):
+
+# print the odds
+        print("i = ",  + i)
