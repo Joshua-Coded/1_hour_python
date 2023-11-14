910f79c597d31c8e5b7ec115cebc9b645401825b
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-14 12:01:59 +0200

    python is cool

--- derek_banas_course/lecture_3.py
+++ derek_banas_course/lecture_3.py
@@ -18,13 +18,20 @@
 # this should be a guessing game between 1  and 10 : 1
 # 
 
+# secret_number = 7
 
-
-secret_number = 7
-
-while True:
-    guess = int(input("Guess the secret number between 1 and 10: "))
+# while True:
+#     guess = int(input("Guess the secret number between 1 and 10: "))
     
-    if guess == secret_number:
-        print("You guessed it right")
-        break
+#     if guess == secret_number:
+#         print("You guessed it right")
+#         break
+
+
+
+sam_string = "This is the best developer on the planet."
+
+print("Lenght: ", len(sam_string))
+
+print(sam_string[0:4])
+
