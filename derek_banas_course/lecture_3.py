0adb013d08ad16b9e5c8b65d0748fe596a979c5f
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-14 11:52:37 +0200

    python is cool

--- derek_banas_course/lecture_3.py
+++ derek_banas_course/lecture_3.py
@@ -1,14 +1,30 @@
-# working with python string
+# # working with python string
+
+# while True:
+    
+#     try:
+#         number = int(input("Enter a number : "))
+#         break
+    
+#     except ValueError:
+#         print("You didn't enter a number")
+        
+#     except:
+#         print("An unknown error occurred")
+# print(number)
+
+
+# problem : implementating do while loop
+# this should be a guessing game between 1  and 10 : 1
+# 
+
+
+
+secret_number = 7
 
 while True:
+    guess = int(input("Guess the secret number between 1 and 10: "))
     
-    try:
-        number = int(input("Enter a number : "))
+    if guess == secret_number:
+        print("You guessed it right")
         break
-    
-    except ValueError:
-        print("You didn't enter a number")
-        
-    except:
-        print("An unknown error occurred")
-print(number)
